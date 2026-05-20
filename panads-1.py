97a387b155e2e5f9c6f91aeb98de3a03b37723ab
student <cxiao7584@gmail.com>
2026-05-20 16:21:57 +0800

    new

---
+++ panads-1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+stock1 = pd.Series([120, 80, None, 60, 95, None, 110])
+index_labels = ["Apple", "Banana", "Orange", "Mango", "Grape", "Peach", "Melon"]
+stock2 = pd.Series([120, 80, None, 60, 95, None, 110], index=index_labels)
+
+stock3 = stock2.to_dict()
+print("stock1")
+print(stock1)
+print("\nstock2")
+print(stock2)
+print("\nstock3")
+print(stock3)
+
+print(f"\nBanana 庫存： {stock2['Banana']}")
+print("\n缺失值檢查：")
+print(stock2.isna())
+print(f"\n缺失值數量： {stock2.isna().sum()}")
+
+stock2.to_csv("0520_stock.csv", header=["Stock"])
